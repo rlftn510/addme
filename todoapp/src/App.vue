032da76bdfa383ae59836d65f96e1694a5267db4
galgale <rlftn510@gmail.com>
2019-05-03 21:46:27 +0900

    컴포넌트 등록 - import 컴포넌트 이름 from 경로 export 에 components 객체 등록 - '컴포넌트 태그명' : 컴포넌트 이름

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
   </div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
 export default {
-  name: 'App'
+
+  components : {
+    //컴포넌트 태그명 : 컴포넌트 내용
+    'todo-header' : TodoHeader,
+    'todo-list' : TodoList,
+    'todo-input' : TodoInput,
+    'todo-footer' : TodoFooter,
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
