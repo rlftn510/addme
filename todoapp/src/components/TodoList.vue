c152496552f98c5909fae637abacb5f8deca7771
galgale <rlftn510@naver.com>
2019-05-06 22:05:41 +0900

    로컬스토리지 데이터 문자화->객체화 로컬스토리지 객체의 변화에 따른 class 변화 v-bind:class

--- todoapp/src/components/TodoList.vue
+++ todoapp/src/components/TodoList.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="fas fa-check checkBtn" v-bind:class="{checkBtnCompleted:todoItem.completed}" 
+        v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fas fa-trash-alt"></i>
         </span>
@@ -23,13 +25,21 @@ export default {
       console.log(todoItem, index)
       localStorage.removeItem(todoItem)
       this.todoItems.splice(index, 1)
+    },
+    toggleComplete(todoItem, index) {
+      todoItem.completed = !todoItem.completed;
+      // 로컬스토리지 데이터 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+      // JSON.stringify == 문자화
     }
   },
   created() {
     if (localStorage.length > 0) {
       for (let i = 0; i < localStorage.length; i++){
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(localStorage.key(i))
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+          // JSON.parse == 객체화
         }
       }
     }
