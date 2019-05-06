9248029d3d5588dfe1eacb57eff16131295b97ff
galgale <rlftn510@gmail.com>
2019-05-06 21:21:36 +0900

    TodoList 할일 리스트 입력 및 삭제 입력 - data에 임시 array todoItems 에 created 를 이용하여 localStorage 의 키 값을 array 에 넣고 template에 li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem"
    삭제
    - methods에 삭제 함수 생성
    localStorage.removeItem(todoItem)
    this.todoItems.splice(index, 1)

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -27,8 +27,18 @@ export default {
 </script>
 
 <style>
-body{text-align: center;background: #fff;}
-input{border-style: groove;width: 200px;}
-button{border-style: none;}
-.shadow{box-shadow: 5px 10px 10px rgba(0,0,0,0.5)}
+body {
+  text-align: center;
+  background-color: #F6F6F8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+}
 </style>
