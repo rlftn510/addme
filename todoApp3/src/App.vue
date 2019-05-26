946ccb494747e405fd8d27f1cf9e7b83c2b74245
galgale <rlftn510@naver.com>
2019-05-26 18:55:04 +0900

    store, vuex 헬퍼등을 사용하지 않은 기본 vue로 동작 세팅

---
+++ todoApp3/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <todoheader></todoheader>
+    <todoinput></todoinput>
+    <todolist></todolist>
+    <todofooter></todofooter>
+  </div>
+</template>
+
+<script>
+
+import todoheader from './components/todoheader.vue';
+import todoinput from './components/todoinput.vue';
+import todolist from './components/todolist.vue';
+import todofooter from './components/todofooter.vue';
+
+
+export default {
+  components : {
+    todoheader,
+    todoinput,
+    todolist,
+    todofooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
