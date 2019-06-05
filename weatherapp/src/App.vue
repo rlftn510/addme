19bfcde9f0f9283a79b4bc71123bd0294a7c84cc
galgale <rlftn510@naver.com>
2019-06-05 17:54:00 +0900

    날씨 api 를 통한 날씨정보 불러오기

---
+++ weatherapp/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <weatherHeader></weatherHeader>
+  </div>
+</template>
+
+<script>
+import weatherHeader from './components/weatherHeader.vue'
+
+export default {
+  components : {
+    weatherHeader
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ weatherapp/src/components/weatherHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Weather Header</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
