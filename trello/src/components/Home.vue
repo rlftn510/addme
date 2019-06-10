ac8ed893c097aa834af9dbb4febb86623dd1862a
galgale <rlftn510@naver.com>
2019-06-10 20:39:50 +0900

    js 및 vue 를 이용한 api 호출

--- trello/src/components/Home.vue
+++ trello/src/components/Home.vue
@@ -2,6 +2,11 @@
   <div>
     Home
     <div>
+      Board List : 
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        Api result : {{apiRes}}
+      </div>
       <ul>
         <li>
           <router-link to="/b/1">board 1</router-link>
@@ -16,7 +21,34 @@
 
 <script>
 export default {
+  data() {
+    return {
+      loading : false,
+      apiRes : ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData(){
+      this.loading = true;
 
+      const req = new XMLHttpRequest()
+      req.open('GET', 'http://localhost:3000/health')
+
+      req.send() // 클라이언트 서버에서 백엔드 서버로 요청
+
+      req.addEventListener('load', () => {
+        this.loading = false;
+        this.apiRes = {
+          status : req.status,
+          statusText : req.statusText,
+          response : JSON.parse(req.response)
+        }
+      })
+    }
+  },
 }
 </script>
 
