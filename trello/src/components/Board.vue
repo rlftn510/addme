4737ef6a2a09ce175e9674c0e017cac64a36fbae
galgale <rlftn510@naver.com>
2019-06-10 18:42:46 +0900

    no message

---
+++ trello/src/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    board
+    <div>bid : {{bid}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bid : 0
+    }
+  },
+  created() {
+    this.bid = this.$route.params.bid
+  },
+}
+</script>
+
+<style>
+
+</style>
--- trello/src/components/Home.vue
+++ trello/src/components/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     Home
+    <div>
+      <ul>
+        <li>
+          <router-link to="/b/1">board 1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">board 2</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
