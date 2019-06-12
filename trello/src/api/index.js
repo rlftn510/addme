ce2dba1f87869ed34ac85cc0e809c4794751a6d1
galgale <rlftn510@naver.com>
2019-06-12 20:17:49 +0900

    보드 생성

--- trello/src/api/index.js
+++ trello/src/api/index.js
@@ -28,7 +28,10 @@ export const setAuthInHeader = token => {
 export const board = {
   fetch(){
     return request('get', '/boards')
-  }
+  },
+  create(title) {
+    return request('post', '/boards', {title})
+  },
 }
 
 export const auth = {
