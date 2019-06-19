63ad42a4cff029fd379df241b4d16211702d27db
galgale <rlftn510@naver.com>
2019-06-19 20:16:47 +0900

    card title & description edit

--- trello/src/components/Card.vue
+++ trello/src/components/Card.vue
@@ -2,14 +2,18 @@
   <modal class="modal-card">
     <div slot="header" class="modal-card-header">
       <div class="modal-card-header-title">
-        <input class="form-control" type="text" :value="card.title" readonly>
+        <input class="form-control" type="text" :value="card.title" 
+        :readonly="!toggleTitle" @click="toggleTitle=true" @blur="onBlurTitle"
+        ref="inputTitle">
       </div>
       <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
     <div slot="body">
       <h3>Description</h3>
       <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
-        readonly v-model="card.description"></textarea>
+        :readonly="!toggleDes" @click="toggleDes=true" @blur="onBlurDes"
+        ref="inputDes"
+         v-model="card.description"></textarea>
     </div>
     <div slot="footer"></div>
   </modal>  
@@ -23,6 +27,12 @@ export default {
   components : {
     Modal
   },
+  data() {
+    return {
+      toggleTitle : false,
+      toggleDes : false
+    }
+  },
   computed: {
     ...mapState({
       card : 'card',
@@ -30,15 +40,33 @@ export default {
     })
   },
   created() {
-    const id = this.$route.params.cid
-    this.FETCH_CARD({id})
+    this.fetchCard()
   },
   methods: {
    ...mapActions([
-     'FETCH_CARD'
+     'FETCH_CARD',
+     'UPDATE_CARD'
    ]),
    onClose() {
      this.$router.push(`/b/${this.board.id}`)
+   },
+   fetchCard() {
+    const id = this.$route.params.cid
+    this.FETCH_CARD({id})
+   },
+   onBlurTitle(){
+    this.toggleTitle = false
+    const title = this.$refs.inputTitle.value.trim()
+    if (!title) return
+    this.UPDATE_CARD({id: this.card.id, title})
+      .then(() => this.fetchCard())
+   },
+   onBlurDes(){
+    this.toggleDes = false
+    const description = this.$refs.inputDes.value.trim()
+    if (!description) return
+    this.UPDATE_CARD({id: this.card.id, description})
+      .then(() => this.fetchCard())
    }
   }
 }
