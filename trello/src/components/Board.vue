f450a85a783d2217792a3106d5913043b35823a4
galgale <rlftn510@naver.com>
2019-06-28 18:46:47 +0900

    보드 타이틀 수정

--- trello/src/components/Board.vue
+++ trello/src/components/Board.vue
@@ -3,7 +3,9 @@
     <div class="board-wrapper">
       <div class="board">
         <div class="board-header">
-          <span class="board-title">{{board.title}}</span>
+          <input type="text" class="form-control" v-if="isEditTitle" v-model="inputTitle"
+           ref="inputTitle" @blur="onSubmitTitle" @keyup.enter="onSubmitTitle">
+          <span v-else class="board-title" @click="onClickTitle">{{board.title}}</span>
           <a class="board-header-btn show-menu" href="" @click.prevent="onShowSettings">
             show Menu
           </a>
@@ -39,6 +41,8 @@ export default {
       bid : 0,
       loading : false,
       cDragger : null,
+      isEditTitle : false,
+      inputTitle : ''
     }
   },
   computed: {
@@ -49,6 +53,7 @@ export default {
   },
   created() {
     this.fetchData().then(() => {
+      this.inputTitle = this.board.title
       this.SET_THEME(this.board.bgColor)
     })
     this.SET_IS_SHOW_BOARD_SETTINGS(false)
@@ -63,8 +68,25 @@ export default {
     ]),
     ...mapActions([
       'FETCH_BOARD',
-      'UPDATE_CARD'
+      'UPDATE_CARD',
+      'UPDATE_BOARD'
     ]),
+    onSubmitTitle() {
+      this.isEditTitle = false
+      this.inputTitle = this.inputTitle.trim()
+      if (!this.inputTitle) return
+
+      const id = this.board.id;
+      const title = this.inputTitle
+      
+      if (title === this.board.title) return
+
+      this.UPDATE_BOARD({id, title})
+    },
+    onClickTitle() {
+      this.isEditTitle = true
+      this.$nextTick(() => this.$refs.inputTitle.focus())
+    },
     fetchData(){
       this.loading = true
       return this.FETCH_BOARD({id:this.$route.params.bid})
